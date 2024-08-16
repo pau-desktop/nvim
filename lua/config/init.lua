6c2f2b2d93c0bdd3eca4f7aa6f76d070351315cb
pau-desktop <pacmanalberti@gmail.com>
2024-08-16 23:17:13 +0200

    Add files via upload

---
+++ lua/config/init.lua
@@ -0,0 +1,55 @@
+-- Set <leader> to <space>
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Lazy installation if not installed
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable",
+        lazypath,
+    })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("config.autocmds")
+require("config.options")
+require("config.keymaps")
+
+local plugins = "plugins"
+
+local opts = {
+    defaults = {
+        -- Will not load plugins by default
+        lazy = true,
+    },
+    install = {
+        colorscheme = { "nightfox" },
+    },
+    rtp = {
+        disabled_plugins = {
+            "gzip",
+            "matchit",
+            "matchparen",
+            "netrw",
+            "netrwPlugin",
+            "tarPlugin",
+            "tohtml",
+            "tutor",
+            "zipPlugin",
+        },
+    },
+    checker = {
+        enabled = true,
+        notify = false,
+	},
+    change_detection = {
+        notify = false,
+    },
+}
+
+require("lazy").setup(plugins,opts)
