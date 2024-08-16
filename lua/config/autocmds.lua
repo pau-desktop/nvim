cc582b4011194b5ffa8c9658f373791c0e81ae99
pau-desktop <pacmanalberti@gmail.com>
2024-08-16 23:16:49 +0200

    Create autocmds.lua

---
+++ lua/config/autocmds.lua
@@ -0,0 +1,7 @@
+    -- Disable auto comment on new line                                                     
+    vim.api.nvim_create_autocmd("FileType", {                                         
+      pattern = "*",                                                                  
+      callback = function()                                                           
+        vim.opt_local.formatoptions:remove({ "r", "o" })                              
+      end,                                                                            
+    })                                                                                
