ec1cc923ac3e2ba101f50fd797009d4db5cebb85
pau-desktop <pacmanalberti@gmail.com>
2024-08-18 23:23:45 +0200

    Create nvim-build.bash

---
+++ nvim-build.bash
@@ -0,0 +1,18 @@
+# Neovim installation from github repo
+sudo apt-get install ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip curl doxygen
+git clone https://github.com/neovim/neovim.git
+cd neovim
+git checkout stable
+make CMAKE_BUILD_TYPE=Release
+sudo make install
+
+# Neovim installation using AppImage
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+chmod u+x nvim.appimage
+./nvim.appimage --appimage-extract
+./squashfs-root/AppRun --version
+
+# Optional: exposing nvim globally.
+sudo mv squashfs-root /
+sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
+nvim
