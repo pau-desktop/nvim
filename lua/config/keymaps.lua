e77e131a07623e4c782c3f9903bddfed8af0671f
pau-desktop <pacmanalberti@gmail.com>
2024-08-19 03:01:06 +0200

    Add files via upload

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,28 @@
+-- Useful keymaps
+vim.keymap.set("n", "G", "G$") -- Start of file
+vim.keymap.set("n", "gg", "gg0") -- End of file
+vim.keymap.set("n", "Y", "yg$") -- Y till eol
+vim.keymap.set("n", "J", "mzJ`z") -- Join lines
+vim.keymap.set("n", "<C-D>", "<C-D>zz") -- Center view
+vim.keymap.set("n", "<C-U>", "<C-U>zz") -- Center view
+vim.keymap.set("n", "n", "nzzzv") -- Center view
+vim.keymap.set("n", "N", "Nzzzv") -- Center view
+vim.keymap.set("n", "Q", "<nop>") -- Disable Q
+vim.keymap.set("i", "<C-c>", "<Esc>") -- Replicate <Esc>
+                                                                                
+-- Buffer management                                                            
+vim.keymap.set("n", "<leader>q", vim.cmd.bd, { desc = "[Q]uit buffer" })    
+vim.keymap.set("n", "<Tab>", vim.cmd.bn) -- Buffer next
+vim.keymap.set("n", "<S-Tab>", vim.cmd.bp) -- Buffer previous
+
+-- Using the void register
+vim.keymap.set("n", "<leader>d", "\"_d", { desc = "[D]elete to void" })
+vim.keymap.set("v", "<leader>d", "\"_d", { desc = "[D]elete to void" })
+vim.keymap.set("x", "<leader>p", "\"_dp", { desc = "[P]aste & repeat" })
+
+vim.keymap.set("v", "<leader>r", "\"hy:%s/<C-r>h/", { desc = "[R]eplace text" })
+
+vim.keymap.set("v", "<", "<gv") -- Indent left
+vim.keymap.set("v", ">", ">gv") -- Indent right
+vim.keymap.set("i", "jk", "<Esc>") -- Escape
+vim.keymap.set("i", "kj", "<Esc>") -- Escape
