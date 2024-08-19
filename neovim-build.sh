8d2f258ded1dcd4acf339b0afc259346f5c8d8c4
pau-desktop <pacmanalberti@gmail.com>
2024-08-19 15:45:34 +0200

    Update neovim-build.sh

--- neovim-build.sh
+++ neovim-build.sh
@@ -1,19 +1,10 @@
-# Dependencies installation
-sudo apt-get install curl ripgrep
+# Dependencies
+sudo apt-get install ripgrep clang
 
 # Neovim installation from github repo
-sudo apt-get install ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip doxygen
+sudo apt-get install ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip curl doxygen
 git clone https://github.com/neovim/neovim.git
 cd neovim
 git checkout stable
 make CMAKE_BUILD_TYPE=Release
 sudo make install
-
-# Neovim installation using AppImage
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
-chmod u+x nvim.appimage
-./nvim.appimage --appimage-extract
-./squashfs-root/AppRun --version
-sudo mv squashfs-root /
-sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
-nvim
